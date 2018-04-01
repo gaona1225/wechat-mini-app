5f70ae50fbd4ef1c56af6bed00d7122a8125d9ed
gaona <gnp1225@163.com>
2018-04-01 14:18:55 +0800

    wechat modify

--- myProject/ui-wechat/pass_config.js
+++ myProject/ui-wechat/pass_config.js
@@ -1,8 +1,8 @@
 // qatestc测试环境域名
-export const hostName='https://wappass.qatest.baidu.com/';
+// export const hostName='https://wappass.qatest.baidu.com/';
 
 // 线上环境域名
-// export let hostName = 'https://wappass.baidu.com/';
+export let hostName = 'https://wappass.baidu.com/';
 
 // 产品线配置的tpl
 export const tpl = 'waimai';
